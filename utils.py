4ffcaf8f354a1e29155667698d3ded8099abbfc4
Maël <mael@bougetb.fr>
2020-08-16 21:12:37 +0200

    add utils.py

---
+++ utils.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+print("import du module utils")
+
+def get_table_list(cursor):
+    _SQL = """SELECT name FROM sqlite_master
+    WHERE type='table'
+    ORDER BY name"""
+    cursor.execute(_SQL)
+    results = cursor.fetchall()
+    table_list = [
+        v[0] for v in results
+        if v[0] != "sqlite_sequence"
+    ]
+    print("voici toutes les tables:", table_list)
+    return table_list
+
+def delete_table(cursor, table):
+    # suppression de la table
+    cursor.execute("DROP TABLE " +table+"")
+    print ("table ", table, " supprimée")
